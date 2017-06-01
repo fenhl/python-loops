b2ff48187a1acaffbc92372e71ac9d62c58a2d28
Fenhl <fenhl@fenhl.net>
2017-06-01 21:03:28 +0000

    Add setup.py and .travis.yml

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    name='loops',
+    description='Convenience classes and functions for looping threads',
+    author='Fenhl',
+    author_email='fenhl@fenhl.net',
+    py_modules=['loops'],
+)
